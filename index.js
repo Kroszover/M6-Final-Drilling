4497d2dc44eff267ac5078a4b55d1feff78baaaa
Camilo Lavado <93832685+Kroszover@users.noreply.github.com>
2023-07-03 22:04:26 -0400

    producto minimo viable funcionando

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
-const port = 8080;
+const hbs = require("handlebars");
+const port = 8090;
 
-app.get("/", (req, res) => {
-  res.send("Página de inicio");
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+
+app.get("/animes", (req, res) => {
+  const animes = require("./data/anime.json");
+  res.render("animes", { animes });
+  console.log(animes);
+});
+
+app.get("/animes/:id", (req, res) => {
+  const id = req.params.id;
+  const anime = require("./data/anime.json")[id];
+  res.render("anime", { anime });
+  console.log(anime);
 });
 
 app.listen(port, () => {
