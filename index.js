a6088bf15bbb2b34b69daab0c0a702413dc28177
Camilo Lavado <93832685+Kroszover@users.noreply.github.com>
2023-07-03 22:18:15 -0400

    estructura de CRUD

--- index.js
+++ index.js
@@ -6,17 +6,35 @@ const port = 8090;
 app.set("view engine", "hbs");
 app.set("views", __dirname + "/views");
 
+//Ruta para ingresar por id
+app.get("/animes/:id", (req, res) => {
+  const id = req.params.id;
+  const anime = require("./data/anime.json")[id];
+  res.render("anime", { anime });
+  console.log(anime.id);
+});
+
+//CRUD
+//Create
+app.get("/animes/create", (req, res) => {
+  res.render("create");
+});
+
+//Read
 app.get("/animes", (req, res) => {
   const animes = require("./data/anime.json");
   res.render("animes", { animes });
   console.log(animes);
 });
 
-app.get("/animes/:id", (req, res) => {
-  const id = req.params.id;
-  const anime = require("./data/anime.json")[id];
-  res.render("anime", { anime });
-  console.log(anime);
+//Update
+app.get("/animes/update", (req, res) => {
+  res.render("update");
+});
+
+//Delete
+app.get("/animes/delete", (req, res) => {
+  res.render("delete");
 });
 
 app.listen(port, () => {
